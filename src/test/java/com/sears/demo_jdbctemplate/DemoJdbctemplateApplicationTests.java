68d1aca35cf862a3fb547b89b9a653abfefed946
John Smith <browntruck246@gmail.com>
2025-03-15 02:25:43 -0500

    init commit

---
+++ src/test/java/com/sears/demo_jdbctemplate/DemoJdbctemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sears.demo_jdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJdbctemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
